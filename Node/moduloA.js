39634bcb30e544522d68840cec0c3a6a64a4942e
Matheus <matheusgobar@gmail.com>
2020-10-05 17:23:05 -0300

    implementando Node.js

---
+++ Node/moduloA.js
@@ -0,0 +1,7 @@
+// modos de exportar // menos comum
+
+this.ola = 'fala pessoal'
+
+exports.bemVindo = 'bem vindo ao node'
+
+module.exports.ateLogo = 'ate o proximo encontro'
---
+++ Node/moduloB.js
@@ -0,0 +1,8 @@
+//modo mais comum de exportar modulos em node
+
+module.exports = {
+    bomDia: 'bom dia',
+    boaNoite(){
+        return 'boa noite'
+    }
+}
