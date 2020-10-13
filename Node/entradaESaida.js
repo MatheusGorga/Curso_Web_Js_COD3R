24bd8883cfaf256c459b95e5d714ffef74dc9f2e
Matheus <matheusgobar@gmail.com>
2020-10-13 01:07:39 -0300

    Exercicios

---
+++ Node/entradaESaida.js
@@ -0,0 +1,18 @@
+const { identity } = require("lodash")
+
+const anonimo = process.argv.indexOf('-a') !== -1
+//console.log(anonimo)
+
+if(anonimo){
+    process.stdout.write('Fala Anonimo!\n')
+    process.exit()
+
+}else{
+    process.stdout.write('informe o nome: ')
+    process.stdin.on('data', data => {
+        const nome = data.toString().replace('\r\n','')
+
+        process.stdout.write(`Fala ${nome}!!\n`)
+        process.exit()
+    })
+}
