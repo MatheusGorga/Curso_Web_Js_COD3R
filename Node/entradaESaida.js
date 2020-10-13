5107506b7eb5abe54a82358d81a3a8670565cedd
Matheus <matheusgobar@gmail.com>
2020-10-13 01:10:18 -0300

    Teste

--- Node/entradaESaida.js
+++ Node/entradaESaida.js
@@ -12,7 +12,7 @@ if(anonimo){
     process.stdin.on('data', data => {
         const nome = data.toString().replace('\r\n','')
 
-        process.stdout.write(`Fala menino ${nome}!!\n`)
+        process.stdout.write(`Fala jovem ${nome}!!\n`)
         process.exit()
     })
 }
